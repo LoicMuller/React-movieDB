958e54f490c12640ed178779370166e058314eb4
Loic Muller <loicdev069@gmail.com>
2021-01-07 17:05:29 +0100

    update Date

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,17 @@
-import React from 'react'
+import React from "react";
 
 function Footer() {
-    return (
-        <footer className="bg-dark p-2 mt-5 text-light text-right footerPage">
-          <div className="container">
-            <p className="lead">Icons are taken from IconFinder.com</p>
-            <p>2020 © Movie Database</p>
-          </div>
-        </footer>
-    )
+  return (
+    <footer className="bg-dark p-2 mt-5 text-light text-right footerPage">
+      <div className="container">
+        <p className="lead">Icons are taken from IconFinder.com</p>
+        <p>
+          {new Date().getUTCFullYear() - 1} - {new Date().getUTCFullYear()} ©
+          Movie Database
+        </p>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
